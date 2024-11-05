6a89fe4fd9442c7ea477a805ac8cfef4c1011d4f
MrTonkTier11 <162076421+MrTonkTier11@users.noreply.github.com>
2024-11-05 19:40:39 +0800

    Add files via upload

---
+++ lab2.cxx
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+/*void arithOperation(){
+    double num_one, num_two;
+    cout << "Please enter num one: \n";
+    cin >> num_one;
+    cout << "Please enter num two: \n";
+    cin >> num_two;
+    cout << "when added: " << num_one +   num_two << endl;
+    cout << "when subtracted: " << num_one -   num_two << endl;
+    cout << "when multiplied: " << num_one *   num_two << endl;
+    cout << "when divided: " << num_one /   num_two << endl;
+}*/
+
+/*void nameSect(){
+       string name, section;
+    cout << "Please enter your name:  ";
+    cin >> name;
+    cout << "Please enter your section: ";
+    cin >> section;
+    cout << "Name: " + name << endl << "Section: " + section;
+}*/
+int main(){
+    //arithOperation();
+    //nameSect();
+    return(0);
+}
